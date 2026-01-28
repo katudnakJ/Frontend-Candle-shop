d627d409a8079bc9bc99433e0844e09170a416b4
kaewyencharm <116154856+noppasornbear@users.noreply.github.com>
2026-01-28 16:28:04 +0700

    add line Icon and facebook Icon to Customer Footer component

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,7 +1,9 @@
 "use client";
 
 import Link from "next/link";
-import { Home, ClipboardList, ShoppingCart, User,Facebook} from "lucide-react";
+import { Home, ClipboardList, ShoppingCart, User } from "lucide-react";
+import LineIcon from "@/components/icon/lineicon";
+import FacebookIcon from "@/components/icon/facebookicon";
 
 const Footer = () => {
   return (
@@ -9,18 +11,29 @@ const Footer = () => {
       {/* --- ส่วนที่ 1: Desktop Footer (แสดงเฉพาะจอใหญ่ md ขึ้นไป) --- */}
       <div className="block bg-cprojectone py-4">
         <div className="container mx-auto px-4 grid grid-cols-1 gap-4 text-sm text-gray-600">
-            
           <div className="ml-auto">
-            
-            <p className="text-black  p-2 ">Line: @candlelight</p>
-            <p className="text-black  p-2 ">Facebook: Candlelight Shop</p>
+            <div className="flex items-center gap-7">
+              <p className="text-black  text-sm">
+                <Link href="https://line.me">
+                  Line: @Candle Shop
+                </Link>
+              </p>
+              <LineIcon size={30} />
+            </div>
+
+            <div className="flex items-center gap-2">
+              <p className="text-black  text-sm ">
+                <Link href="https://www.facebook.com/">
+                  Facebook: Candle Shop
+                </Link>
+              </p>
+              <FacebookIcon size={30} />
+            </div>
           </div>
         </div>
       </div>
       <div className="bg-white py-2 border-t">
-        <p className="text-center text-xs text-gray-400">
-          © 2026 Candle Shop.
-        </p>
+        <p className="text-center text-xs text-gray-400">© 2026 Candle Shop.</p>
       </div>
     </footer>
   );
