fa730c895aafb845746640d435aec291109db882
KatudnakJ <katudnak_j@su.ac.th>
2026-02-02 10:40:10 +0700

    [KAN-14] save draft

---
+++ src/service/example/example.type.ts
@@ -0,0 +1,11 @@
+export interface LineLiffLoginProps {
+    lineToken: string;
+    lineID: string;
+    name: string;
+    img: string;
+}
+
+// export interface lineLiffResponse {
+//   status: LineLiffLoginProps
+//   data: string
+// }
