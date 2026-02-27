d656d70685aaff51f3f43005d21c929047d1ba11
kaewyencharm <116154856+noppasornbear@users.noreply.github.com>
2026-02-27 14:27:50 +0700

    create SellerHeader component and implement sellerhome page

---
+++ src/app/sellerhome/page.tsx
@@ -0,0 +1,56 @@
+import Head from "next/head";
+
+import ProductCard from "@/modules/products/components/ProductCard";
+import { mockProducts } from "@/modules/products/mockdata";
+import Header from "@/components/layout/CustomerHeader";
+
+import SellerHeader from "@/components/layout/SellerHeader";
+import Footer from "@/components/layout/Footer";
+import { ListFilter } from "lucide-react";
+import CustomerWelcome from "@/modules/customers/components/CustomerWelcome";
+
+export default function sellerHome() {
+
+ 
+
+
+  return (
+    <div className="flex flex-col min-h-screen">
+      {/* Header*/}
+      <SellerHeader />
+
+      <main className="flex-grow bg-white font-sans">
+        {/* Welcome Customer*/}
+
+        <div className="bg-cprojectone">
+          <section >
+         
+          </section>
+        </div>
+
+        <div className="bg-cprojectone w-full min-h-screen">
+          <div className="max-w-[1200px] mx-auto p-4 space-y-8 bg-white ">
+            {/* สินค้าแนะนำ      */}
+
+            <section>
+        
+            </section>
+
+            {/* สินค้าทั้งหมด      */}
+
+            <section>
+         
+            </section>
+
+                <section>
+         
+            </section>
+          </div>
+        </div>
+      </main>
+      {/* Footer*/}
+
+      <Footer />
+    </div>
+  );
+}
