f21dff69bc1069d4a96821379a6ac42a8e0e7a03
kaewyencharm <116154856+noppasornbear@users.noreply.github.com>
2026-04-06 13:33:48 +0700

    Fix: update footer links and improve styling for better user experience

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -4,30 +4,37 @@ import Link from "next/link";
 import LineIcon from "@/components/icon/lineicon";
 import FacebookIcon from "@/components/icon/facebookicon";
 
-const shopname = "Moji's candle shop"
+const shopname = "Moji's candle shop";
 const Footer = () => {
   return (
     <footer className="w-full bg-cprojectone ">
-    
       <div className="max-w-300 mx-auto py-6 px-4 block ">
         <div className=" grid grid-cols-1 gap-4 text-sm text-gray-600">
           <div className="ml-auto">
             <div className="flex items-center gap-7 mr-auto">
-              <p className="text-black text-xs md:text-sm">
-                <Link href="https://line.me" target="_blank">
+              <p className=" text-black text-xs md:text-sm hover:font-bold">
+                <Link
+                  href="https://lin.ee/RYKgjN0"
+                  target="_blank"
+                  className="flex items-center"
+                >
                   Line: @{shopname}
+                  <LineIcon size={30} />
                 </Link>
               </p>
-              <LineIcon size={30} />
             </div>
 
             <div className="flex items-center gap-2 mr-auto">
-              <p className="text-black  text-xs md:text-sm ">
-                <Link href="https://www.facebook.com/candlemoji" target="_blank">
+              <p className="text-black  text-xs md:text-sm hover:font-bold">
+                <Link
+                  href="https://www.facebook.com/candlemoji"
+                  target="_blank"
+                  className="flex items-center"
+                >
                   Facebook: {shopname}
+                  <FacebookIcon size={30} />
                 </Link>
               </p>
-              <FacebookIcon size={30} />
             </div>
           </div>
         </div>
